fb5b0fad62db639e15b37933e039cb358e20aefe
Atom <100242617+Atomarverseucht@users.noreply.github.com>
2024-11-01 00:41:56 +0100

    Add the first classes (UI & calculator)

---
+++ UserInface.java
@@ -0,0 +1,22 @@
+//package MathProgramme;
+
+import java.util.Scanner;
+/**
+ * Histogramm liest ganze Zahlen zwischen 1 und 6 ein und
+ * gibt deren H&auml;ufigkeitsverteilung als Histogramm aus.
+ * @author Atom
+ * @version 0
+ */
+public final class UserInface {
+    private UserInface() { }
+
+    private static final Scanner INPUT = new Scanner(System.in);
+
+    /**
+     * main ist der Startpunkt des Programms.
+     * @param args wird nicht verwendet.
+     */
+    public static void main(String[] args) {
+        System.out.println(Calculator.test);
+    }
+}
