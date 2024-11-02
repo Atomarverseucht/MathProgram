b0a443020c5d93f546de93fb3e37b1800b889546
Atom <atomcraft.4u@web.de>
2024-11-02 02:25:35 +0100

    still merge

--- MathProgram/UserInterface.java
+++ MathProgram/UserInterface.java
@@ -4,11 +4,10 @@ package MathProgram;
 import java.util.Scanner;
 
 /**
- * Histogramm liest ganze Zahlen zwischen 1 und 6 ein und
- * gibt deren H&auml;ufigkeitsverteilung als Histogramm aus.
+ * MathProgram should calculate mathematical terms (accepted complex numbers), today it's only showing the numbers in the cartesian form.
  * @author Atom
  * @author Guakocius
- * @version 01.11.2024
+ * @version 02.11.2024/1
  */
 public final class UserInterface {
     private UserInterface() { }
@@ -21,24 +20,32 @@ public final class UserInterface {
      * @param args isn't used.
      */
     public static void main(String[] args) {
-
+        int count = 1;
         MathProgram.Number number = new Number(0,0);
+
         // Scans in the user written complex numbers and the term operator
         // to calculate them and print them out
         System.out.println("\nComplex Numbers: a + ib");
         System.out.println("Please enter in the real part of the first number: ");
-        number.real = INPUT.nextDouble();
-    	System.out.println("Please put in the imaginary part: ");
-        number.imaginary = INPUT.nextDouble();
-        System.out.println("Please put in a term operator: ");
-        operator = INPUT.next();
-        Calculator.number.add(number);
-        Calculator.operator.add(operator);
+        while (INPUT.hasNext()) {
+            number.real = INPUT.nextDouble();
+    	    System.out.println("Please put in the imaginary part: ");
+            number.imaginary = INPUT.nextDouble();
+            System.out.println("Please put in a term operator: ");
+            operator = INPUT.next();
+            Calculator.number.add(number);
+            Calculator.operator.add(operator);++count;
+            System.out.println("Please enter in the real part of the " + count + ". number: ");
+        }
 
         // TODO: Find an elegant way to display imaginary numbers because of i being a char
         // TODO and imaginary being a double, use i or a library which puts i into a complex
         // TODO: number or implement it ourselves.
         // System.out.println("The result is: " + numbers[0].real + term + numbers[0].imaginary + "i");
-
+        count = 0;
+        for (Number value: Calculator.number) {
+            System.out.println(count + ". number: " + value.real +"+"+ value.imaginary + "i" + Calculator.operator.get(count));
+            ++count;
+        }
     }
 }
